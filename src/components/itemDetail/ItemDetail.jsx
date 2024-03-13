3669add9cecb6fbeeb5477738a75327d223d7761
emablanco1800 <emablanco1800@gmail.com>
2024-03-12 21:17:10 -0300

    alineando ItemDetail

--- src/components/itemDetail/ItemDetail.jsx
+++ src/components/itemDetail/ItemDetail.jsx
@@ -3,7 +3,7 @@ import ItemCount from "../itemCount/ItemCount";
 
 const ItemDetail = ({producto}) => {
     return(
-        <div>
+        <div className='d-flex flex-column align-items-center justify-content-around'>
             <h3>Detalle de: {producto.name} </h3>
             <img alt={producto.name} src={producto.imagen}/>
             <p>{producto.description}</p>
